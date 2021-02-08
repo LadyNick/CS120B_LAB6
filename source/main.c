86cfe5ea42ea28ff90063e78eb60f44673d2ec2e
LadyNick <nnikki1013@gmail.com>
2021-02-07 16:38:50 -0800

    ex 3 coding

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: Nicole Navarro
  *  Partner(s) Name: 
  *	Lab Section: 21
- *	Assignment: Lab #6  Exercise #2
+ *	Assignment: Lab #6  Exercise #3
  *	Demo Link: https://youtu.be/gZMcOekCVO0
  *	Exercise Description: [optional - include for your own benefit]
  *
@@ -15,98 +15,119 @@
 #include "timer.h"
 #endif
 
-enum light_states{start, lights, pause, cont} light_state;
+enum count_states{init, inc_p, inc_r, dec_p, dec_r, reset_p, reset_r, release} count_state;
 
-unsigned char tracker = 0x00;
-unsigned char count = 0x00;
+//unsigned char not_pina;
 unsigned char A0;
+unsigned char A1;
 
-void Tick(){
-    switch(light_state){
-	case start:
-		light_state = lights;
-		break;
-	case lights:
-		if(A0){
-			light_state = pause;
-		}
-		else{
-		if(tracker == 0){
-			PORTB = 0x01;
-		}
-		if(tracker == 1){
-			PORTB = 0x02;
-		}
-		if(tracker == 2){
-			PORTB = 0X04;
-		}
-		
-		++tracker;
-		if(tracker > 2){
-			tracker = 0;
-		}
-	
-		light_state = lights;
-		}
-		break;
-	case pause:
-		if(A0 && (count == 1)){
-			count = 0;
-			light_state = cont;
-		}
-		else if(A0){
-			light_state = pause;
-		}
-		else if(!A0){ 
-			count = 1;
-			light_state = pause;
-		}
-   		break;
-	case cont:
-		if(A0 && (count == 1)){
-			count = 0;
-			light_state = pause;
-		}
-		else{
-		if(tracker == 0){
-			PORTB = 0x01;
-		}
-		if(tracker == 1){
-			PORTB = 0x02;
-		}
-		if(tracker == 2){
-			PORTB = 0X04;
-		}
-		++tracker;
-		if(tracker > 2){
-			tracker = 0;
-		}
-		if(!A0){
-			count = 1;
-		}
-		light_state = cont;
-		}
-		break; 
-	default:
-		light_state = lights;
-		break;
-    }
+void Tick_Count(){
+	switch (count_state){
+		case init:
+			PORTB = 0X07;
+			if( A0 && !A1 ){
+				count_state = inc_p;
+			}
+			else if( !A0 && A1){
+				count_state = dec_p;
+			}
+			else if( A0 && A1 ){
+				count_state = reset_p;
+			}
+			else{
+				count_state = init;
+			}
+			break;
+		case inc_p:
+			if((PORTB < 0X09)){
+				PORTB = PORTB + 1;
+			}
+			if( A0 && A1 ){
+				count_state = reset_p;
+			}
+			else{
+				count_state = inc_r;
+			}
+			break;
+		case inc_r: 
+			if( A0 && A1 ){
+				count_state = reset_p;
+			}
+			else if( A0 && !A1 ){
+				count_state = inc_r;
+			}
+			else if( !A0 && !A1 ){
+				count_state = release;
+			}
+			break;
+		case dec_p:
+			if((PORTB > 0)){
+				PORTB = PORTB - 1;
+			}
+			if( A0 && A1 ) {
+				count_state = reset_p;
+			}
+			else{
+				count_state = dec_r;
+			}
+			break;
+		case dec_r:
+			if(A0 && A1 ){
+				count_state = reset_p;
+			}
+			else if( A1 && !A0){
+				count_state = dec_r;
+			}
+			else if( !A1 && !A0){
+				count_state = release;
+			}
+		case reset_p:
+			PORTB = 0x00;
+			count_state = reset_r;
+			break;
+		case reset_r:
+			if( !A0 && !A1 ){
+				count_state = release;
+			}
+			else{
+				count_state = reset_r;
+			}
+			break;
+		case release:
+			if( A0 && !A1 ){
+                                count_state = inc_p;
+                        }
+                        else if( !A0  && A1){
+                                count_state = dec_p;
+                        }
+                        else if( A0 && A1 ){
+                                count_state = reset_p;
+                        }
+			else{
+				count_state = release;	
+			}
+                        break;
+		default:
+			count_state = init;
+			break;
+	}
 }
 
 int main(void) {
     DDRB = 0XFF;   PORTB = 0x00;
     DDRA = 0X00;   PINA = 0XFF; 
-    TimerSet(300);
+    TimerSet(100);
     TimerOn();
 	
-    	light_state = lights;	
+    	count_state = init;
+	PORTC = 0x07;	
 
     while (1) {
 	A0 = ~PINA & 0X01;
-	Tick();
-	while(!TimerFlag); //Wait 1 sec
+	A1 = ~PINA & 0X02
+	Tick_Count();
+	while(!TimerFlag); 
 	TimerFlag = 0;
-	//Better style would use synchSM with TickSM, this exmaple just illustrates the use of the ISR and flag
     }
     return 1;
 }
